946a3d5e3392adc7d2f0a72cd93fa654e10d69ae
Ray Myers <ray.myers@divvydose.com>
2022-03-10 19:30:04 -0600

    Extract methods in RoomDisplayer

--- src/main/java/org/gamenet/kata/untestable/RoomDisplayer.java
+++ src/main/java/org/gamenet/kata/untestable/RoomDisplayer.java
@@ -16,38 +16,56 @@ public class RoomDisplayer {
     private final IRoomService roomService;
 
     public void createAndDisplayRooms() {
-        for (int index = 0; index < 40; index++) {
-            roomService.createRandomRoom();
-        }
+        createInitialRooms();
 
         System.out.println("initial rooms");
 
-        List<BasicRoom> roomList = roomService.getRoomList();
-        for (BasicRoom room : roomList) {
-            roomService.displayRoom(room);
-        }
+        displayRooms(roomService.getRoomList());
 
-        ObjectQuery<BasicRoom> roomQuery = new ObjectQuery<>(BasicRoom.class);
-        roomQuery.add("width", Integer.valueOf(5));
-        List<BasicRoom> filteredRoomList = null;
-        try {
-            filteredRoomList = roomService.getFilteredRoomList(roomQuery);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            // We should never reach here
-        }
+        ObjectQuery<BasicRoom> roomQuery = buildWidthQuery();
+        List<BasicRoom> filteredRoomList = getFilteredRoomListFailingSilently(roomQuery);
 
-        for (Iterator<BasicRoom> it = filteredRoomList.iterator(); it.hasNext(); ) {
+        removeBlueRooms(filteredRoomList);
+
+        System.out.println("filtered rooms");
+
+        displayRooms(filteredRoomList);
+    }
+
+    void removeBlueRooms(List<BasicRoom> rooms) {
+        for (Iterator<BasicRoom> it = rooms.iterator(); it.hasNext(); ) {
             BasicRoom basicRoom = it.next();
             ColoredRoom coloredRoom = (ColoredRoom)basicRoom;
             if (Displayable.Color.BLUE == coloredRoom.getColor()) {
                 it.remove();
             }
         }
+    }
 
-        System.out.println("filtered rooms");
+    List<BasicRoom> getFilteredRoomListFailingSilently(ObjectQuery<BasicRoom> roomQuery) {
+        try {
+            return roomService.getFilteredRoomList(roomQuery);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            // We should never reach here
+        }
+        return null;
+    }
 
-        for (BasicRoom room : filteredRoomList) {
+    ObjectQuery<BasicRoom> buildWidthQuery() {
+        ObjectQuery<BasicRoom> roomQuery = new ObjectQuery<>(BasicRoom.class);
+        roomQuery.add("width", Integer.valueOf(5));
+        return roomQuery;
+    }
+
+    void displayRooms(List<BasicRoom> rooms) {
+        for (BasicRoom room : rooms) {
             roomService.displayRoom(room);
+        }
+    }
+
+    void createInitialRooms() {
+        for (int index = 0; index < 40; index++) {
+            roomService.createRandomRoom();
         }
     }
 
