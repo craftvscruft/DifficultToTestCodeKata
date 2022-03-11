378443cc6d2cea37d7b40035082bfb4fcfbb56e6
Ray Myers <ray.myers@divvydose.com>
2022-03-10 20:11:36 -0600

    Test build width query

--- src/test/java/org/gamenet/kata/untestable/RoomDisplayTest.java
+++ src/test/java/org/gamenet/kata/untestable/RoomDisplayTest.java
@@ -1,6 +1,12 @@
 package org.gamenet.kata.untestable;
 
+import org.gamenet.kata.unmodifiable.objectFilter.ObjectQuery;
+import org.gamenet.kata.unmodifiable.rooms.BasicRoom;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 public class RoomDisplayTest {
     @Test
@@ -11,5 +17,21 @@ public class RoomDisplayTest {
         subject.createInitialRooms();
 
         assertEquals(40, roomService.randomRoomsCreated);
+    }
+
+    @Test
+    public void buildWidthQueryFiltersForWidth5() throws Exception {
+        FakeRoomService roomService = new FakeRoomService();
+        RoomDisplayer subject = new RoomDisplayer(roomService);
+
+        ObjectQuery<BasicRoom> query = subject.buildWidthQuery();
+        BasicRoom foundRoom = new BasicRoom();
+        foundRoom.width = 5;
+        BasicRoom otherRoom = new BasicRoom();
+        otherRoom.width = 4;
+        List<BasicRoom> foundRooms = query.getObjects(Arrays.asList(foundRoom, otherRoom));
+
+        assertEquals(1, foundRooms.size());
+        assertSame(foundRoom, foundRooms.get(0));
     }
 }
